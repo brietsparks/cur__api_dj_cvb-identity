6b9591b714596bd3cccb007bae249596538e4636
brietsparks <bws46@cornell.edu>
2017-05-15 21:44:11 -0500

    changed project name to users_service

--- users_service/urls.py
+++ users_service/urls.py
@@ -1,4 +1,4 @@
-"""cvb_identity URL Configuration
+"""users_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
